e278d59523f87862848821a797a4a85609a21ab9
Michael Wahba <mrswahba13@gmail.com>
2021-10-18 10:59:18 -0700

    Add files via upload

---
+++ Source/Neo4jConnector/Private/Neo4jNode.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Neo4jNode.h"
+
+
+//iterates through nodeProperties array and prints all properties
+/*
+FString UNeo4jNode::GetStringProperty(FString propertyName)
+{
+
+	auto val = nodeProperties.Find(propertyName);
+	return val->ToSharedRef()->AsString();
+}
+
+int UNeo4jNode::GetIntProperty(FString propertyName)
+{
+	auto val = nodeProperties.Find(propertyName);
+	return val->ToSharedRef()->AsNumber();
+}
+
+bool UNeo4jNode::GetBoolProperty(FString propertyName)
+{
+	auto val = nodeProperties.Find(propertyName);
+	return val->ToSharedRef()->AsBool();
+}
+
+FString UNeo4jNode::SerializeNode()
+{
+	
+}
+*/
