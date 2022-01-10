12a1ffb35acac367435710ad606403e6b13a7e8c
Michael Wahba <mrswahba13@gmail.com>
2022-01-10 11:53:38 -0700

    added error checking for null node properties

--- Source/Neo4jConnector/Private/Neo4jFilters.cpp
+++ Source/Neo4jConnector/Private/Neo4jFilters.cpp
@@ -10,22 +10,48 @@
 int UNeo4jFilters::FilterIntProperty(FNeo4jNode inNode, FString property)
 {
 
-	return inNode.properties.Find(property)->ToSharedRef()->AsNumber();
+	
+	if(inNode.properties.Contains(property))
+	{
+		return inNode.properties.Find(property)->ToSharedRef()->AsNumber();
+	}
+
+	UE_LOG(LogTemp,Error,TEXT("Error trying to access integer property: %s"),*property)
+	return -1;
 
 }
 
 FString UNeo4jFilters::FilterStringProperty(FNeo4jNode inNode, FString property)
 {
-	return inNode.properties.Find(property)->ToSharedRef()->AsString();
+	if(inNode.properties.Contains(property))
+	{
+		return inNode.properties.Find(property)->ToSharedRef()->AsString();
+		
+	}
+	
+	UE_LOG(LogTemp,Error,TEXT("Error trying to access string property: %s"),*property)
+	return "null";
 }
 
 bool UNeo4jFilters::FilterBoolProperty(FNeo4jNode inNode, FString property)
 {
-	return inNode.properties.Find(property)->ToSharedRef()->AsBool();
+	if(inNode.properties.Contains(property))
+	{
+		return inNode.properties.Find(property)->ToSharedRef()->AsBool();
+	}
+	
+	
+	UE_LOG(LogTemp,Error,TEXT("Error trying to access bool property: %s"),*property)
+	return false;
+	
 }
 
 float UNeo4jFilters::FilterFloatProperty(FNeo4jNode inNode, FString property)
 {
-	return inNode.properties.Find(property)->ToSharedRef()->AsNumber();
+	if(inNode.properties.Contains(property))
+		return inNode.properties.Find(property)->ToSharedRef()->AsNumber();
+	
+	UE_LOG(LogTemp,Error,TEXT("Error trying to access float property: %s"),*property)
+	return -1.0f;
 }
 
